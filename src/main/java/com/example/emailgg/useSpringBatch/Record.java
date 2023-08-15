ad001ca60f5057ccd1e306036d31f8e3c5210026
qvdev-source <hienquoc412018@gmail.com>
2023-08-15 22:21:11 +0700

    batch version

---
+++ src/main/java/com/example/emailgg/useSpringBatch/Record.java
@@ -0,0 +1,11 @@
+package com.example.emailgg.useSpringBatch;
+
+import lombok.Data;
+
+@Data
+public class Record {
+    private String oldUrl;
+    private String newUrl;
+    // ... các thuộc tính khác, getter và setter
+}
+
